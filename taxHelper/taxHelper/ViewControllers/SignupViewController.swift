6584d7c21d80de89257cc3c3966dac789dbf2b8b
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2020-12-10 12:08:39 -0500

    my ios app called taxHelper

--- taxHelper/taxHelper/ViewControllers/SignupViewController.swift
+++ taxHelper/taxHelper/ViewControllers/SignupViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBOutlet weak var returnToLoginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,4 +41,16 @@ class SignupViewController: UIViewController {
     }
     */
 
+    @IBAction func signupTapped(_ sender: Any) {
+    }
+    
+    @IBAction func returnToLoginTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
 }
