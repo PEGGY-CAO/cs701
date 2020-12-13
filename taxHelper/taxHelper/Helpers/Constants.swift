f142d34135f1db305cbd15027116bc8675456a8e
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2020-12-13 11:57:42 -0500

    Google firebase login auth, store info in database

---
+++ taxHelper/taxHelper/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  taxHelper
+//
+//  Created by Yuqi Cao on 12/10/20.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
